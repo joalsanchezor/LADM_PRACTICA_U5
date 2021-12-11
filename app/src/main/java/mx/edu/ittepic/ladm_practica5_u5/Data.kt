9603b8c3a375b8a400aa6021ea5e9f82cbffaca0
[joalsanchezor] <[joalsanchezor@ittepic.edu.mx]>
2021-12-11 10:23:23 -0700

    2nd commit

--- app/src/main/java/mx/edu/ittepic/ladm_practica5_u5/Data.kt
+++ app/src/main/java/mx/edu/ittepic/ladm_practica5_u5/Data.kt
@@ -1,11 +1,15 @@
 package mx.edu.ittepic.ladm_practica5_u5
 
+import android.content.Context
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.GeoPoint
 
 class Data {
     var nombre: String = ""
     var posicion1: GeoPoint = GeoPoint(0.0,0.0)
     var posicion2: GeoPoint = GeoPoint(0.0,0.0)
+    var baseRemota = FirebaseFirestore.getInstance()
 
     override fun toString(): String {
         return nombre+"\n"+posicion1.latitude+","+posicion1.longitude+"\n"+
@@ -24,5 +28,27 @@ class Data {
 
     private fun invertir(valor:Double):Double{
         return valor*-1
+    }
+
+    fun lugares(context: Context): ArrayList<Data>{
+        var lugares = ArrayList<Data>()
+        baseRemota.collection("Lugares")
+            .addSnapshotListener { value, error ->
+                if(error != null){
+                    Toast.makeText(context, "ERROR: ${error.message}", Toast.LENGTH_LONG).show()
+                    return@addSnapshotListener
+                }
+                lugares.clear()
+                for(document in value!!){
+                    var data = Data()
+                    data.nombre = document.getString("nombre").toString()
+                    data.posicion1 = document.getGeoPoint("posicion1")!!
+                    data.posicion2 = document.getGeoPoint("posicion2")!!
+
+                    lugares.add(data)
+                }
+            }
+
+        return lugares
     }
 }
--- app/src/main/java/mx/edu/ittepic/ladm_practica5_u5/MapsActivity.kt
+++ app/src/main/java/mx/edu/ittepic/ladm_practica5_u5/MapsActivity.kt
@@ -1,10 +1,12 @@
 package mx.edu.ittepic.ladm_practica5_u5
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.LocationServices
 
@@ -14,6 +16,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.GeoPoint
 import kotlinx.android.synthetic.main.activity_maps.*
 import mx.edu.ittepic.ladm_practica5_u5.databinding.ActivityMapsBinding
@@ -22,6 +25,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    var baseRemota = FirebaseFirestore.getInstance()
+    var lugares = ArrayList<Data>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +36,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         //Ocultar ActionBar
         getSupportActionBar()?.hide()
+
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
@@ -43,8 +49,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 1)
         }
 
+        getLugares()
+
         btnUbicacion.setOnClickListener {
             Toast.makeText(this, "UBICACIÓN", Toast.LENGTH_LONG).show()
+            miUbicacion()
         }
     }
 
@@ -98,7 +107,37 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
         LocationServices.getFusedLocationProviderClient(this)
             .lastLocation.addOnSuccessListener {
-                //var geoPosicion = GeoPoint(it.latitude, it.longitude)
+                var geoPosicion = GeoPoint(it.latitude, it.longitude)
+                for(item in lugares){
+                    if(item.estoyEn(geoPosicion)){
+                        AlertDialog.Builder(this)
+                            .setMessage("SE ENCUENTRA EN ${item.nombre}")
+                            .setTitle("ATENCIÓN")
+                            .setPositiveButton("OK"){p, q->}
+                            .show()
+                    }
+                }
+            }.addOnFailureListener {
+                Toast.makeText(this,"ERROR AL OBTENER UBICACIÓN", Toast.LENGTH_LONG).show()
+            }
+    }
+
+    fun getLugares(){
+        baseRemota.collection("Lugares")
+            .addSnapshotListener { value, error ->
+                if(error != null){
+                    Toast.makeText(this, "ERROR: ${error.message}", Toast.LENGTH_LONG).show()
+                    return@addSnapshotListener
+                }
+                lugares.clear()
+                for(document in value!!){
+                    var data = Data()
+                    data.nombre = document.getString("nombre").toString()
+                    data.posicion1 = document.getGeoPoint("posicion1")!!
+                    data.posicion2 = document.getGeoPoint("posicion2")!!
+
+                    lugares.add(data)
+                }
             }
     }
 
