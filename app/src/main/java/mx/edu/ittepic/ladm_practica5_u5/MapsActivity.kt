a1574a4a2c41e76083dcbf376906b3d0751b7eb4
[joalsanchezor] <[joalsanchezor@ittepic.edu.mx]>
2021-12-10 21:41:50 -0700

    Initial commit

---
+++ app/src/main/java/mx/edu/ittepic/ladm_practica5_u5/MapsActivity.kt
@@ -0,0 +1,105 @@
+package mx.edu.ittepic.ladm_practica5_u5
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.LocationServices
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.firestore.GeoPoint
+import kotlinx.android.synthetic.main.activity_maps.*
+import mx.edu.ittepic.ladm_practica5_u5.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Ocultar ActionBar
+        getSupportActionBar()?.hide()
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        if(ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this,
+            arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 1)
+        }
+
+        btnUbicacion.setOnClickListener {
+            Toast.makeText(this, "UBICACIÓN", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera: defecto -34.0, 151.0
+        //21.510791, -104.899410
+
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        LocationServices.getFusedLocationProviderClient(this)
+            .lastLocation.addOnSuccessListener {
+                //var geoPosicion = GeoPoint(it.latitude, it.longitude)
+                val tepic = LatLng(it.latitude, it.longitude)
+                mMap.addMarker(MarkerOptions().position(tepic).title("Mi ubicación"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(tepic))
+
+                mMap.uiSettings.isZoomControlsEnabled = true
+            }
+    }
+
+    private fun miUbicacion(){
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        LocationServices.getFusedLocationProviderClient(this)
+            .lastLocation.addOnSuccessListener {
+                //var geoPosicion = GeoPoint(it.latitude, it.longitude)
+            }
+    }
+
+}
